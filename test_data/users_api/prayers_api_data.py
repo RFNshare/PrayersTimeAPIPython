2abd6b4dee189c4be0cb7c8aab64e08b38fae79c
abdullahalfaroque <af.qups@gmail.com>
2021-12-20 16:56:56 +0600

    prayer times API 5,6,7

--- test_data/users_api/prayers_api_data.py
+++ test_data/users_api/prayers_api_data.py
@@ -23,6 +23,21 @@ def response_get_times_this_month():
     res = data_from_server("GET", this_month)
     return res.json(), res.status_code
 
+
+def response_get_times_this_dates():
+    res = data_from_server("GET", this_dates)
+    return res.json(), res.status_code
+
+
+def response_get_times_this_day():
+    res = data_from_server("GET", day)
+    return res.json(), res.status_code
+
+
+def response_get_times_month():
+    res = data_from_server("GET", month)
+    return res.json(), res.status_code
+
 #
 # def test():
 #     """returns response and status for login with only email password"""
